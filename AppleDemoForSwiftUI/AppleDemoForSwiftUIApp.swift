d25726d3f215fc84050d1e9edacfca09c25ca222
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-08 11:29:27 +0530

    Initial Commit

---
+++ AppleDemoForSwiftUI/AppleDemoForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleDemoForSwiftUIApp.swift
+//  AppleDemoForSwiftUI
+//
+//  Created by Knoxpo MacBook Pro on 08/04/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleDemoForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
